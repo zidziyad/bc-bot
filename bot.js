d26a936da44039e2ab1a2c1ff37801f034614302
zidziyad <43486451+zidziyad@users.noreply.github.com>
2019-11-30 21:52:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,31 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = ['d'];
 const request = require("request");
-const ytdl = require("ytdl-core");
-const canvas = require("canvas");
-const Canvas = require("canvas");
-const convert = require("hh-mm-ss")
-const fetchVideoInfo = require("youtube-info");
-const botversion = require('./package.json').version;
-const simpleytapi = require('simple-youtube-api')
-const moment = require("moment");
-const fs = require('fs');
-const util = require("util")
-const gif = require("gif-search");
-const opus = require("node-opus");
-const ms = require("ms");
-const jimp = require("jimp");
-const { get } = require('snekfetch');
-const guild = require('guild');
-const dateFormat = require('dateformat');//npm i dateformat
-const YouTube = require('simple-youtube-api');
-const youtube = new YouTube('AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8');
-const hastebins = require('hastebin-gen');
-const getYoutubeID = require('get-youtube-id');
-const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const pretty = require("pretty-ms");
-const queue = new Map();
-var table = require('table').table
 console.log("Scrpit By zizo");
 
 
