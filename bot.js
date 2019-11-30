0926cd6c194e8b7e7df4c785eb09d24a818a494d
zidziyad <43486451+zidziyad@users.noreply.github.com>
2019-11-30 20:28:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -132,14 +132,7 @@ client.on("message", message => {
 client.on("message", message => {
   var prefix = "d";
   var cats = [
-    "http://palestine-kitchen.ps/wp-content/uploads/2017/12/%D9%86%D9%83%D8%AA-%D8%AF%D8%A8%D8%A7%D9%86%D8%A9.png",
-    "http://www.i7lm.com/wp-content/uploads/2017/04/136769797816.jpg",
-    "https://4.bp.blogspot.com/-p62zmDIDXmI/WKzqNt9smaI/AAAAAAAAC4Q/sW_bSIB8OaQhwOYFeplc3uzz8PBN7l3YACEw/s1600/13602501135.jpg",
-    "https://www.universemagic.com/images/2016/03/7938-2-or-1457539273.jpg",
-    "https://1.bp.blogspot.com/-yFk-FzHSyE8/WR9fmPcsCUI/AAAAAAAAE6c/AmvjLadOiLY9GiCqMLHgA121bY2RS_dCwCLcB/s1600/%25D9%2586%25D9%2583%25D8%25AA%2B%25D9%2585%25D8%25B6%25D8%25AD%25D9%2583%25D8%25A9%2B1.jpg",
-    "https://l7zaat.com/wp-content/uploads/2018/02/423.jpg",
-    "https://www.petfinder.com/wp-content/uploads/2012/11/101438745-cat-conjunctivitis-causes.jpg",
-    "http://www.shuuf.com/shof/uploads/2018/02/08/jpg/shof_97d686082bdb0a2.jpg"
+    "https://dog.ceo/api/breeds/image/random"
   ];
   var args = message.content.split(" ").slice(1);
   if (message.content.startsWith(prefix + "dog")) {
